6d424cceb0be00015c99ed99115bceb62676c02e
evanZccc <167677714+evanZccc@users.noreply.github.com>
2024-11-13 00:30:51 +0100

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     token: '',
-    guildId: '1239288643907092521',
+    guildId: '<1239288643907092521>',
     mongodbURI: 'mongodb+srv://evanz:<EvanZccc2000>@cluster0.kx6iu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 };
